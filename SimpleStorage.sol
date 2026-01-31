c99d5a912889e0ee05ff2cca38419d5637b9585a
Tanishqchahal <chahat5@mcmaster.ca>
2026-01-31 05:16:31 -0500

    simple storage factory

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -8,9 +8,42 @@ contract SimpleStorage{
     // int256 favoriteInt = -88;
     // address myAddress = 0xC2cE398F7C2115fbfe5EA80b3Fe92630092CeD12;
     // bytes32 favoriteBytes32 = "cat";    
-    uint256 public favoriteNumber; //by default initialised to 0, by default visibilty is internal
+    uint256 myfavoriteNumber; //by default initialised to 0, by default visibilty is internal
+    
+    // Lists
+    // uint256[] listOffFavoriteNumbers;
+
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // Person public myFriend = Person(6, "Harshit");
+
+    // Person public Harshit = Person({favoriteNumber: 6, name:"Harshit"});
+    // Person public Hardik = Person({favoriteNumber: 7, name:"Hardik"});
+    // Person public Megh = Person({favoriteNumber: 8, name:"Megh"});
+
+    Person[] public listOfPeople; //dynamic array
+    // Person[n] public listOfPeople; //static array of size n
+
+    mapping(string => uint256) public nameToFavoriteNumber;
 
     function store(uint256 _fav) public {
-        favoriteNumber = _fav;
+        myfavoriteNumber = _fav;
+    }
+
+    // view, pure
+    function retrieve() public view returns(uint256){
+        return myfavoriteNumber;
+    }
+
+    //calldata, memory, storage
+    //calldata is temporary variables that cannot be modified.
+    //memory is temporary variables that can be modified.
+    //storage is permanent variables that can be modified.
+    function addPerson(uint256 _fav, string memory _name) public{
+        listOfPeople.push(Person(_fav, _name));
+        nameToFavoriteNumber[_name] = _fav;
     }
 }
---
+++ StorageFactory.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+contract StorageFactory{
+
+    //type visiblity name
+    SimpleStorage[] public listOfSimpleStorage;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorageContract = new SimpleStorage();
+        listOfSimpleStorage.push(simpleStorageContract);
+    }
+
+    function sfstore(uint256 _simpleStorageIndex, uint256 _newFavNum) public {
+        SimpleStorage mySimpleStorage = listOfSimpleStorage[_simpleStorageIndex];
+        mySimpleStorage.store(_newFavNum);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        SimpleStorage mySimpleStorage = listOfSimpleStorage[_simpleStorageIndex];
+        return mySimpleStorage.retrieve();
+    }
+}
